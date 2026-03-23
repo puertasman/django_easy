9216dbfebd73987efa4f5bb82d0dbe6261e9011c
puertasman <puertasman@gmail.com>
2026-03-23 16:38:00 +0100

    cambio visible

--- config/config/views.py
+++ config/config/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hola Enrique, Django está funcionando correctamente 
+    return HttpResponse("Hola Enrique, Django está funcionando correctamente.")
